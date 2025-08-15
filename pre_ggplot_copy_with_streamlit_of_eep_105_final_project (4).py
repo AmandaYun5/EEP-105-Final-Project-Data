2ca095b1fdc676872de910cf73c62813c3e894b8
AmandaYun5 <amandayun5@gmail.com>
2025-08-15 16:05:22 -0700

    Update pre_ggplot_copy_with_streamlit_of_eep_105_final_project (4).py

--- pre_ggplot_copy_with_streamlit_of_eep_105_final_project (4).py
+++ pre_ggplot_copy_with_streamlit_of_eep_105_final_project (4).py
@@ -767,7 +767,7 @@ Where y refers to temperature statistics
 #Define a function for converting to standard units
 def std_units(array):
     return (array - np.mean(array))/np.std(array)
-test = 'def std_units(array): return (array - np.mean(array))/np.std(array)')
+test = 'def std_units(array): return (array - np.mean(array))/np.std(array)'
 st.code(test)
 
 #Convert dataframe back to narrow version 
